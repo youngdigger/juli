bf67cc5095721f353578afd8fa9c5c47aca078ee
Giovanni <gigialf2006@gmail.com>
2024-10-13 21:59:28 -0500

    make it better

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Servir archivos estáticos
+app.use(express.static(path.join(__dirname)));
+
+// Ruta principal
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
